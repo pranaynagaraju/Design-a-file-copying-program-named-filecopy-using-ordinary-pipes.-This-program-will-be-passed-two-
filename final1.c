62d0300faa219d30a435768af2541b37ec3299a2
pranaynagaraju <58062796+pranaynagaraju@users.noreply.github.com>
2020-04-16 23:24:03 +0530

    Update final1.c

--- final1.c
+++ final1.c
@@ -1,67 +1,64 @@
-#include <stdio.h>
-#include <string.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <fcntl.h>
-
-
-int main(int argc, char *argv[])
+#include <stdio.h>
+#include <string.h>
+int main(int arg1, char *arg2[])
 {
-    int fd[2];
-	int fileSize;
-	int pid;
+    int filedescriptor[2]; 
+	int fileSize; 
+               int x; 
 
-    char text[1024];
+    char text[1024];  
 	char copy[1024];  
-
-    char* sourceFile = argv[1];
-    char* destFile = argv[2];
+    char* sourceFile = arg2[1]; 
+    char* destFile = arg2[2]; 
 
   
 
-    pipe(fd);
-    if (pipe(fd)==-1) 
+    pipe(filedescriptor);
+    if (pipe(filedescriptor)==-1)
     { 
-        printf(stderr, "Pipe Failed" ); 
+        printf(stderr, "Pipe Failed" );
         return 1; 
     }  
  
-    pid = fork();
-     if (pid < 0) 
+    x = fork();// complexity=1
+     if (x < 0) 
     { 
-        printf(stderr, "Fork Failed" ); 
+        printf(stderr, "Child Failed" ); 
         return 1; 
     } 
 
-    if (pid > 0) 
+    if (x > 0) 
     {
 
-	   int sourceFileNum;
+	   int sourceFileNum; 
 	   ssize_t numBytes; 
 
-        close(fd[0]);
-        sourceFileNum=open(sourceFile, O_RDONLY);
-        numBytes=read(sourceFileNum, text,sizeof(text));
+        close(filedescriptor[0]);
+        sourceFileNum=open(sourceFile, O_RDONLY); 
+        numBytes=read(sourceFileNum, text,sizeof(text)); 
 
-        write(fd[1],text, numBytes);
+        write(filedescriptor[1],text, numBytes); 
   
-        close(fd[1]);
+        close(filedescriptor[1]);
 
-    }else if (pid == 0){
+    }else if (x == 0){
     	
     	int destDesc;
 
-        close(fd[1]);
-        ssize_t numBytesCh;
-        numBytesCh =read(fd[0], copy,sizeof(copy));
+        close(filedescriptor [1]); 
+        ssize_t numBytesCh; 
+        numBytesCh =read(filedescriptor[0], copy,sizeof(copy));
 
-        close(fd[0]);
-    
-        destDesc=open(destFile, O_CREAT | O_WRONLY);
+        close(filedescriptor[0]);
+	    
+        destDesc=open(destFile, O_CREAT | O_WRONLY); 
 
-        write(destDesc, copy, numBytesCh);
+        write(destDesc, copy, numBytesCh); 
     }
 
-    return 0;
-}  
+    return 0; // complexity=1
+}
